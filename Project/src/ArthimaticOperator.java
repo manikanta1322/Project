03336f30f3fd8e5e536cdec5bd8bb131ff1c962b
DELL <DELL@ManikantaChemiti>
2022-06-02 12:10:59 +0530

    first commit

---
+++ Project/src/ArthimaticOperator.java
@@ -0,0 +1,26 @@
+
+public class ArthimaticOperator {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		
+		num1++;
+		num1 = num1 + 1;
+		
+		num1--;
+		num1 = num1 - 1;
+		
+		num1+=5;
+		num1 = num1 + 5;
+		
+		num1-=2;
+		num1 = num1 - 5;
+		
+		num1*=4;
+		System.out.println(num1);
+		
+				
+
+	}
+
+}
