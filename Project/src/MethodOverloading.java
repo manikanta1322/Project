ac67d087f2ce1f530b20d2db94804a1e4a117a01
DELL <DELL@ManikantaChemiti>
2022-06-02 12:16:59 +0530

    first commit

---
+++ Project/src/MethodOverloading.java
@@ -0,0 +1,32 @@
+
+public class MethodOverloading
+{
+	int add(int a,int b)
+	{
+		int sum=a+b;
+		return sum;
+	}
+	float add(float a,float b)
+	{
+		return a+b;
+	}
+	float add(int a,float b,int c)
+	{
+		return a+b+c;
+	}
+	float add(float a,int b,int c)
+	{
+		return a+b+c;
+	}
+	public static void main(String[] args) {
+		MethodOverloading obj=new
+				MethodOverloading();
+		System.out.println(obj.add(10,10));
+		System.out.println(obj.add(3.45f,5.67f));
+		System.out.println(obj.add(10,20.22f,67));
+		System.out.println(obj.add(24.67f,20,100));
+		
+
+	}
+
+}
