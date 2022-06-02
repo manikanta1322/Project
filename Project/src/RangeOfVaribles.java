f63c077b18915755cfd0a09f3fef144769e855a2
DELL <DELL@ManikantaChemiti>
2022-06-02 12:24:47 +0530

    second commit

---
+++ Project/src/RangeOfVaribles.java
@@ -0,0 +1,23 @@
+
+public class RangeOfVaribles {
+
+	public static void main(String[] args) {
+		byte maxbyte=127;
+		byte minbyte=-128;
+		System.out.println("Min range of byte is" + minbyte +"Max range of byte is" + maxbyte);
+		short maxshort = 32767;
+		short minshort = -32768;
+		System.out.println("Minshort range of byte is" + minshort + "Maxshort range of byte is"+ maxshort);
+		int maxint = 214748647;
+		int minint = -214748648;
+		System.out.println("Minint range of byte is" + minint + "Maxint range of byte is" + maxint);
+		long maxlong = 9223372036854775807L;
+		long minlong = -9223372036854775808L;
+		System.out.println("Minlong range of byte is" + minlong + "Maxlong range of byte is" + maxlong);
+		float f = 3234.141243278345f;
+		double d = 3456.1412451234567890234567894f;
+		System.out.println("float value is"+f+"double value is"+d);
+
+	}
+
+}
